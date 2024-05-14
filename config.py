9d65318b63ad7606850e43d1b266379cbdf249bf
Martin Hyca <55054100+martin-hyca@users.noreply.github.com>
2024-05-14 20:20:28 +0200

    basic files upload

---
+++ config.py
@@ -0,0 +1,28 @@
+import numpy as np
+import os
+
+# actions = np.array(['Hi', 'ILoveYou', '_'])
+# original value ['Hi', 'Yes', 'No', 'ThankYou', 'ILoveYou', 'background', 'NoHands']
+# - not changed yet, just writing it down here since I think we need to have a more flexible solution for changing the list of names... (?)
+
+actions = np.array(['Hi', 'Yes', 'No', 'ThankYou', 'ILoveYou', 'background', 'NoHands'])
+
+facemesh_included = False
+
+is_martin = False
+
+# Path for exported data, numpy arrays
+# DATA_PATH = os.path.join('mediapipe_keypoints')
+DATA_PATH = os.path.join('MP_Data')
+# model weights folder; default: subfolder "weights"
+WEIGHTS_PATH = os.path.join("weights")
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+number_of_classes = len(actions)
+
+
